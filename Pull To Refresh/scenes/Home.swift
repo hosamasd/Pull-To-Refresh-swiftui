3399b5ae5bf15a813523c0e991acafa4aec31d75
hosam mohamed <hosamelmalt@gmail.com>
2020-12-05 09:06:26 -0800

    done refresh

---
+++ Pull To Refresh/scenes/Home.swift
@@ -0,0 +1,68 @@
+//
+//  Home.swift
+//  Pull To Refresh
+//
+//  Created by Hossam on 12/5/20.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    // sample Data....
+    
+    @State var arrayData = ["Hello Data 1","Hello Data 2","Hello Data 3","Hello Data 4","Hello Data 5"]
+   
+    
+    var body: some View{
+        
+        VStack(spacing: 0){
+            
+            HStack{
+                
+                Text("Hosam")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.purple)
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color.white.ignoresSafeArea(.all, edges: .top))
+            
+            Divider()
+            
+            KRefreshScrollView(progressTint: .purple, arrowTint: .purple) {
+                
+                VStack{
+                    
+                    ForEach(arrayData,id: \.self){value in
+                        
+                        HStack{
+                            
+                            Text(value)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.black)
+                        }
+                        .padding()
+                    }
+                }
+                .background(Color.white)
+                .padding()
+                
+            } onUpdate: {
+                
+                arrayData.append("New Data")
+            }
+
+        }
+        .background(Color.black.opacity(0.06).ignoresSafeArea())
+    }
+    
+   
+    
+}
+
