f2d362206da66432fab57ca2ecfa4459add1e4d5
hosam mohamed <hosamelmalt@gmail.com>
2020-12-05 08:37:53 -0800

    Initial Commit

---
+++ Pull To Refresh/Pull_To_RefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pull_To_RefreshApp.swift
+//  Pull To Refresh
+//
+//  Created by Hossam on 12/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Pull_To_RefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
